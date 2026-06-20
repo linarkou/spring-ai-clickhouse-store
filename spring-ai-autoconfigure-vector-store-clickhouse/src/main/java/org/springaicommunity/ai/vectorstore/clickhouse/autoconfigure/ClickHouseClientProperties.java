a227492271083fd3118b66d3ba133a11f6ade74d
Linar Abzaltdinov <abzaltdinov@gmail.com>
2026-06-20 17:36:01 +0300

    Spring AI 2 & Spring Boot 4 support: - bump ClickHouse client to 0.9.8 - Jackson v3 support - jspecify @Nullable annotations added

--- spring-ai-autoconfigure-vector-store-clickhouse/src/main/java/org/springaicommunity/ai/vectorstore/clickhouse/autoconfigure/ClickHouseClientProperties.java
+++ spring-ai-autoconfigure-vector-store-clickhouse/src/main/java/org/springaicommunity/ai/vectorstore/clickhouse/autoconfigure/ClickHouseClientProperties.java
@@ -1,23 +1,8 @@
-/*
- * Copyright 2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springaicommunity.ai.vectorstore.clickhouse.autoconfigure;
 
 import java.time.Duration;
 import java.util.*;
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -32,26 +17,52 @@ public class ClickHouseClientProperties {
 
     private Set<String> endpoints = new HashSet<>();
 
+    @Nullable
     private String username;
+
+    @Nullable
     private String password;
+
+    @Nullable
     private String accessToken;
 
+    @Nullable
     private String defaultDatabaseName;
 
+    @Nullable
     private Boolean sslAuthentication;
 
+    @Nullable
     private String sslTrustStorePath;
+
+    @Nullable
     private String sslTrustStorePassword;
+
+    @Nullable
     private String sslTrustStoreType;
 
+    @Nullable
     private String rootCertificatePath;
+
+    @Nullable
     private String clientCertificatePath;
+
+    @Nullable
     private String clientKeyPath;
 
+    @Nullable
     private Duration connectTimeout;
+
+    @Nullable
     private Duration connectionRequestTimeout;
+
+    @Nullable
     private Duration connectionTtl;
+
+    @Nullable
     private Duration socketTimeout;
+
+    @Nullable
     private Duration executionTimeout;
 
     private boolean useMeterRegistry = true;
@@ -76,131 +87,147 @@ public class ClickHouseClientProperties {
         this.endpoints.add(endpoint);
     }
 
+    @Nullable
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(@Nullable String username) {
         this.username = username;
     }
 
+    @Nullable
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(@Nullable String password) {
         this.password = password;
     }
 
+    @Nullable
     public String getAccessToken() {
         return accessToken;
     }
 
-    public void setAccessToken(String accessToken) {
+    public void setAccessToken(@Nullable String accessToken) {
         this.accessToken = accessToken;
     }
 
+    @Nullable
     public String getDefaultDatabaseName() {
         return defaultDatabaseName;
     }
 
-    public void setDefaultDatabaseName(String defaultDatabaseName) {
+    public void setDefaultDatabaseName(@Nullable String defaultDatabaseName) {
         this.defaultDatabaseName = defaultDatabaseName;
     }
 
+    @Nullable
     public Boolean getSslAuthentication() {
         return sslAuthentication;
     }
 
-    public void setSslAuthentication(Boolean sslAuthentication) {
+    public void setSslAuthentication(@Nullable Boolean sslAuthentication) {
         this.sslAuthentication = sslAuthentication;
     }
 
+    @Nullable
     public String getSslTrustStorePath() {
         return sslTrustStorePath;
     }
 
-    public void setSslTrustStorePath(String sslTrustStorePath) {
+    public void setSslTrustStorePath(@Nullable String sslTrustStorePath) {
         this.sslTrustStorePath = sslTrustStorePath;
     }
 
+    @Nullable
     public String getSslTrustStorePassword() {
         return sslTrustStorePassword;
     }
 
-    public void setSslTrustStorePassword(String sslTrustStorePassword) {
+    public void setSslTrustStorePassword(@Nullable String sslTrustStorePassword) {
         this.sslTrustStorePassword = sslTrustStorePassword;
     }
 
+    @Nullable
     public String getSslTrustStoreType() {
         return sslTrustStoreType;
     }
 
-    public void setSslTrustStoreType(String sslTrustStoreType) {
+    public void setSslTrustStoreType(@Nullable String sslTrustStoreType) {
         this.sslTrustStoreType = sslTrustStoreType;
     }
 
+    @Nullable
     public String getRootCertificatePath() {
         return rootCertificatePath;
     }
 
-    public void setRootCertificatePath(String rootCertificatePath) {
+    public void setRootCertificatePath(@Nullable String rootCertificatePath) {
         this.rootCertificatePath = rootCertificatePath;
     }
 
+    @Nullable
     public String getClientCertificatePath() {
         return clientCertificatePath;
     }
 
-    public void setClientCertificatePath(String clientCertificatePath) {
+    public void setClientCertificatePath(@Nullable String clientCertificatePath) {
         this.clientCertificatePath = clientCertificatePath;
     }
 
+    @Nullable
     public String getClientKeyPath() {
         return clientKeyPath;
     }
 
-    public void setClientKeyPath(String clientKeyPath) {
+    public void setClientKeyPath(@Nullable String clientKeyPath) {
         this.clientKeyPath = clientKeyPath;
     }
 
+    @Nullable
     public Duration getConnectTimeout() {
         return connectTimeout;
     }
 
-    public void setConnectTimeout(Duration connectTimeout) {
+    public void setConnectTimeout(@Nullable Duration connectTimeout) {
         this.connectTimeout = connectTimeout;
     }
 
+    @Nullable
     public Duration getConnectionRequestTimeout() {
         return connectionRequestTimeout;
     }
 
-    public void setConnectionRequestTimeout(Duration connectionRequestTimeout) {
+    public void setConnectionRequestTimeout(@Nullable Duration connectionRequestTimeout) {
         this.connectionRequestTimeout = connectionRequestTimeout;
     }
 
+    @Nullable
     public Duration getConnectionTtl() {
         return connectionTtl;
     }
 
-    public void setConnectionTtl(Duration connectionTtl) {
+    public void setConnectionTtl(@Nullable Duration connectionTtl) {
         this.connectionTtl = connectionTtl;
     }
 
+    @Nullable
     public Duration getSocketTimeout() {
         return socketTimeout;
     }
 
-    public void setSocketTimeout(Duration socketTimeout) {
+    public void setSocketTimeout(@Nullable Duration socketTimeout) {
         this.socketTimeout = socketTimeout;
     }
 
+    @Nullable
     public Duration getExecutionTimeout() {
         return executionTimeout;
     }
 
-    public void setExecutionTimeout(Duration executionTimeout) {
+    public void setExecutionTimeout(@Nullable Duration executionTimeout) {
         this.executionTimeout = executionTimeout;
     }
 
